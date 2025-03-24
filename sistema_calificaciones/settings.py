5697e78aa56eeb4ce86a92dc098cd848c8303081
Victor <victor.blancofigueroa@cesunbc.edu.mx>
2025-03-24 11:50:18 -0700

    Modificacion de inicio y agregacion de apis

--- sistema_calificaciones/settings.py
+++ sistema_calificaciones/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,7 +121,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# settings.py
+
+STATIC_URL = '/static/'
+
+# Añadir aquí el directorio donde están los archivos estáticos
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # Esto hace que busque en la carpeta static en la raíz de tu proyecto
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
